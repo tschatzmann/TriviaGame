9543f62cda9e01b43a9cba8041e649d6c00f52dc
tschatzmann <tarrta2002@gmail.com>
2019-01-03 21:49:57 -0500

    last javascript try

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -68,91 +68,93 @@ function buildQuestion() {
                 );
             }
 
-            output.push(
-                `<div class="slide">
-        <div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>
-        </div>`
-            );
-            console.log(output)
+            // output.push(
+            //    `<div class="slide">
+            //  <div class="question"> ${currentQuestion.question} </div>
+            //<div class="answers"> ${answers.join("")} </div>
+            //</div>`
+            //);
+
 
             // add this question and its answers to the output
             output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-      <div class="answers"> ${answers.join('')} </div>`
+                `<div class="slide">
+                 <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>
+            </div>`
             );
-        }
-    );
-
-    // finally combine our output list into one string of HTML and put it on the page
-    questionContainer.innerHTML = output.join('');
-}
+            console.log(output);
+            // finally combine our output list into one string of HTML and put it on the page
+            questionContainer.innerHTML = output.join('');
+    })
+};
 
 
 function showpossibleAnswers() {
 
-    // gather answer containers from our questions
-    const answerContainers = questionContainer.querySelectorAll('.answers');
+            // gather answer containers from our questions
+            const answerContainers = questionContainer.querySelectorAll('.answers');
 
-    // keep track of user's answers
-    let numCorrect = 0;
+            // keep track of user's answers
+            let numCorrect = 0;
 
-    // for each question...
-    gameQuestions.forEach((currentQuestion, questionNumber) => {
+            // for each question...
+            gameQuestions.forEach((currentQuestion, questionNumber) => {
 
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = 'input[name=question' + questionNumber + ']:checked';
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+                // find selected answer
+                const answerContainer = answerContainers[questionNumber];
+                const selector = 'input[name=question' + questionNumber + ']:checked';
+                const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-        // if answer is correct
-        if (userAnswer === currentQuestion.correctAnswer) {
-            // add to the number of correct answers
-            numCorrect++;
+                // if answer is correct
+                if (userAnswer === currentQuestion.correctAnswer) {
+                    // add to the number of correct answers
+                    numCorrect++;
 
-            // color the answers green
-            answerContainers[questionNumber].style.color = 'lightgreen';
+                    // color the answers green
+                    answerContainers[questionNumber].style.color = 'lightgreen';
+                }
+                // if answer is wrong or blank
+                else {
+                    // color the answers red
+                    answerContainers[questionNumber].style.color = 'red';
+                }
+            });
+
+            // show number of correct answers out of total
+            possibleAnswersContainer.innerHTML = numCorrect + ' out of ' + gameQuestions.length;
+        };
+    function showSlide(n) {
+        console.log(slides)
+        //document.getElementsByClassName(slide).classList.add('active-slide')
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if (currentSlide === 0) {
+            previousButton.style.display = 'none';
         }
-        // if answer is wrong or blank
         else {
-            // color the answers red
-            answerContainers[questionNumber].style.color = 'red';
+            previousButton.style.display = 'inline-block';
         }
-    });
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        }
+        else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    };
+    showSlide(0);
 
-    // show number of correct answers out of total
-    possibleAnswersContainer.innerHTML = numCorrect + ' out of ' + gameQuestions.length;
-};
-function showSlide(n) {
-    console.log(slides)
-    slides[currentSlide].classList.remove('active-slide');
-    slides[n].classList.add('active-slide');
-    currentSlide = n;
-    if (currentSlide === 0) {
-        previousButton.style.display = 'none';
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
     }
-    else {
-        previousButton.style.display = 'inline-block';
-    }
-    if (currentSlide === slides.length - 1) {
-        nextButton.style.display = 'none';
-        submitButton.style.display = 'inline-block';
-    }
-    else {
-        nextButton.style.display = 'inline-block';
-        submitButton.style.display = 'none';
-    }
-}
-showSlide(0);
 
-function showNextSlide() {
-    showSlide(currentSlide + 1);
-}
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
 
-function showPreviousSlide() {
-    showSlide(currentSlide - 1);
-}
-
-previousButton.addEventListener("click", showPreviousSlide);
-nextButton.addEventListener("click", showNextSlide);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
 
