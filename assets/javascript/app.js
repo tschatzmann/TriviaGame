3177eeea264aaa3088350fae08fde405a7f09000
tschatzmann <tarrta2002@gmail.com>
2019-01-16 11:41:57 -0500

    clean up code, add css and html for header

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,19 @@
 const questionContainer = document.getElementById('triviaQuestions');
 const startGamebtn = document.getElementById('startGame');
-const gameTimer = document.getElementById('timeDisplay')
+const gameTimer = document.getElementById('timeDisplay');
+const correctAnswersDisplay = document.getElementById('correctAnswersDisplay');
 
 var intervalId = "";
 var timerRunning = false;
-var time = 0;
 var questionNumber = -1;
 var searchWord = "";
 var questionMessage = "";
 let numCorrect = 0;
 let numWrong = 0;
-let seconds30 = 15;
+let seconds5 = 5;
 let minutes = 0;
 let seconds = 0;
+let answerText = '';
 
 
 const gameQuestions = [
@@ -45,82 +46,101 @@ const gameQuestions = [
             c: "School",
         },
         correctAnswer: "a",
-        GiffSearchWord: "pig"
+        GiffSearchWord: "baby pigs"
     },
+    {
+        question: "What animal has the longest lifespan?",
+        answers: {
+            a: "Elephant",
+            b: "Blue Whale",
+            c: "Giant Tortoise",
+            d: "Locust",
+        },
+        correctAnswer: "c",
+        GiffSearchWord: "Giant Tortoise"
+    },
+    {
+        question: "What is the fastest flying bird in the world?",
+        answers: {
+            a: "Harpy Eagle",
+            b: "Peregrine Falcon",
+            c: "Horned Sungem",
+            d: "Spine-tailed Swift",
+        },
+        correctAnswer: "b",
+        GiffSearchWord: "Peregrine Falcon"
+    },
+    {
+        question: " What is the largest of the great apes?",
+        answers: {
+            a: "Orangutan",
+            b: "Western Lowland Gorilla",
+            c: "Eastern Lowland Gorilla",
+            d: "Mountain Gorilla",
+        },
+        correctAnswer: "d",
+        GiffSearchWord: "Mountain Gorilla"
+    }
 ];
 
 var numberOfQuestions = (Object.keys(gameQuestions));
-console.log(numberOfQuestions);
 numberOfQuestions = numberOfQuestions.length++;
-console.log(numberOfQuestions);
 questionContainer.innerHTML = "Select Start to begin the game";
 // start the games
 startGamebtn.addEventListener('click', initGame);
 
 //
 function initGame() {
-    console.log(gameQuestions);
     questionContainer.innerHTML = "";
+    correctAnswersDisplay.innerHTML = "";
     startGamebtn.style.display = "none";
     gameTimer.innerText = "Countdown: 00:00";
-    time = 0;
     timer = 0;
     stopTimer();
-    seconds30 = 15;
-    startTimer(seconds30,gameTimer);
+    startTimer(seconds5, gameTimer);
     questionNumber++;
     buildQuestion();
-    //setTimeout(checkAnswers, 1000 * 15);
 };
 
 function startTimer(duration, gameTimer) {
-        if (!timerRunning) {
-            timerRunning = true;
-            var timer = duration, minutes, seconds;
-            intervalId =  setInterval(function () {
-                console.log(intervalId)
+    if (!timerRunning) {
+        timerRunning = true;
+        var timer = duration, minutes, seconds;
+        intervalId = setInterval(function () {
             minutes = parseInt(timer / 60, 10)
             seconds = parseInt(timer % 60, 10);
-    
+
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
-    
+
             gameTimer.innerText = "Countdown: " + minutes + ":" + seconds;
-    
+
             if (--timer < 0) {
                 timer = duration;
+                checkAnswers();
             }
         }, 1000);
-    } else{
-        stopTimer();
     }
 };
 function stopTimer() {
-   // DONE: Use clearInterval to stop the count here and set the clock to not be running.
-   clearInterval(intervalId);
+    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+    clearInterval(intervalId);
     timerRunning = false;
 };
 
 
 function buildQuestion() {
     if (questionNumber < numberOfQuestions) {
-        console.log(questionNumber);
-        console.log(gameQuestions.lenght + 1);
-        // we'll need a place to store the HTML output
         const output = [];
         var currentQuestion = gameQuestions[questionNumber];
-        console.log(currentQuestion);
         questionContainer.innerHTML = output;
-
         // for each question...
-        console.log('questionNumber ' + questionNumber);
-
-        // we'll want to store the list of answer choices
+        // store the list of answer choices, seaerch word and answer text
         const answers = [];
         const gifWords = [];
+        answerText = '';
         // and for each available answer...
         for (letter in currentQuestion.answers) {
-
             // ...add an HTML radio button
             answers.push(
                 `<button class="ansbuttons"
@@ -131,15 +151,13 @@ function buildQuestion() {
           </button>`
             );
         }
-        console.log(answers);
         output.push(
 
             `<div class="question"> ${currentQuestion.question} </div>
            <div id="answerbuttons"> ${answers.join("")} </div>`
         );
         gifWords.push(currentQuestion.GiffSearchWord);
-
-        console.log('output ' + output);
+        answerText = (currentQuestion.answers[currentQuestion.correctAnswer])
         questionContainer.innerHTML = output.join("");
     }
     else {
@@ -148,54 +166,37 @@ function buildQuestion() {
 
 };
 
-$(questionContainer).on('click',".ansbuttons", function() {
-    console.log('click answer buttons');
-    console.log(this);
+$(questionContainer).on('click', ".ansbuttons", function () {
     var userAnswer = $(this).attr("answer")
-    console.log(userAnswer);
     checkAnswers(userAnswer)
 });
 
-function endGame() {
-    var gameoverTemplate = `
-        <p> Your score is ${numCorrect} out of ${gameQuestions.length}`;
-    questionContainer.innerHTML = gameoverTemplate;
-    // show number of correct answers out of total
-    stopTimer();
-    startGamebtn.style.display = "initial";
-    startGamebtn.textContent= "Restart";
-    questionNumber = -1;
-    numCorrect = 0;
-    numWrong = 0;
-    time = 0;
-};
+
 
 function checkAnswers(ans) {
-stopTimer();
+    stopTimer();
     // for each question...
     var currentQuestion = gameQuestions[questionNumber];
-    console.log(questionNumber);
-    console.log(currentQuestion);
     questionContainer.innerHTML = "  ";
-
     searchWord = currentQuestion.GiffSearchWord;
-     console.log('button userAnswer ' + ans);
     // if answer is correct
     if (ans === currentQuestion.correctAnswer) {
         // add to the number of correct answers
         numCorrect++;
-        questionMessage = `You are correct. The answer is ${currentQuestion.correctAnswer}`
+        questionMessage = `You are correct. The answer is ${answerText}`
     }
     // if answer is wrong or blank
     else {
         numWrong++;
-    questionMessage = ` That is incorrect. The correct answer is ${currentQuestion.correctAnswer} `
+        questionMessage = ` That is incorrect. The correct answer is ${answerText} `
     }
-
     displayCorrectAnswer(searchWord);
-    // startTimer(seconds30,gameTimer);
-    setTimeout(initGame,1000 * 5 );
- };
+    startTimer(seconds5, gameTimer);
+    //sleep between questions
+    sleep(3000).then(() => {
+        initGame();
+    })
+};
 
 function displayCorrectAnswer(searchWord) {
     // Example queryURL for Giphy API
@@ -204,8 +205,6 @@ function displayCorrectAnswer(searchWord) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        console.log(response.data[0].url);
         var results = response.data[0];
         // Only taking action if the photo has an appropriate rating
         if (results.rating !== "r" && results.rating !== "pg-13") {
@@ -213,10 +212,31 @@ function displayCorrectAnswer(searchWord) {
             var ptemplate = `<p>
                 ${questionMessage}</p>
                 <div>
-                <img src="${ results.images.fixed_height.url }">
+                <img src="${ results.images.fixed_height.url}">
                 </div>`
-            questionContainer.innerHTML = ptemplate;
-
+            correctAnswersDisplay.innerHTML = ptemplate;
         }
     });
+};
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+};
+
+function endGame() {
+    var gameoverTemplate = `
+        <p> Your score is ${numCorrect} out of ${gameQuestions.length}`;
+    questionContainer.innerHTML = gameoverTemplate;
+    // show number of correct answers out of total
+    stopTimer();
+    // reset vars
+    startGamebtn.style.display = "initial";
+    startGamebtn.textContent = "Restart";
+    questionNumber = -1;
+    numCorrect = 0;
+    numWrong = 0;
+    time = 0;
+    minutes = 0;
+    seconds = 0;
+    questionContainer.innerHTML = "Select Start to begin the game";
 };
